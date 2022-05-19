f194219d9044ae52d35d7bf0e779d0e996f645e7
Saha <2102068@cognizant.com>
2022-05-19 14:53:09 +0530

    deleted

--- employee-microservice/src/main/java/com/cts/employeemicroservice/repository/OfferRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cts.employeemicroservice.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.cts.employeemicroservice.model.EmployeeOffers;
-
-public interface OfferRepository extends CrudRepository<EmployeeOffers, Integer> {
-
-}
